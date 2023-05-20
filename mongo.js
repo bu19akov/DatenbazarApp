18e6a072e04b6b9602268a7d561261d2e0588a4d
Vladimir Bulgakov <a12132881@unet.univie.ac.at>
2023-05-20 19:00:09 +0200

    test

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://vovabulgakov00:f5VAMsdUpKqMG71p@cluster0.aiux5xb.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
   serverApi: {
