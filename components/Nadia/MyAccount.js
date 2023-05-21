b707c093cd168c35f7383b9ffad2dd2ff35bd80a
Nadezhda Tsvetkova <nadezhda.tsvetkova23@gmail.com>
2023-05-21 13:25:06 +0200

    changed design and added warning message

--- components/Nadia/MyAccount.js
+++ components/Nadia/MyAccount.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, Alert } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, Alert, Dimensions } from 'react-native';
 import { MaterialIcons, Ionicons } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function MyAccount() {
   const navigation = useNavigation();
@@ -24,88 +26,98 @@ export default function MyAccount() {
     );
   };
 
+  const handleDataSellTransactions = () => {
+    Alert.alert(
+      "Under Development",
+      "This feature is currently under development. Please check back later.",
+      [{ text: "OK" }]
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.titleContainer}>
-          <Text style={styles.title}>My Account</Text>
-          <TouchableOpacity onPress={() => navigation.navigate('AccountSettings')}>
-              <Ionicons name="settings" size={24} color="black" />
+      <View style={[styles.header, { width: SCREEN_WIDTH }]}>
+        <TouchableOpacity onPress={() => navigation.navigate('AccountSettings')}>
+          <MaterialIcons name="arrow-back" size={40} color="black" />
+        </TouchableOpacity>
+        <Text style={styles.title}>My Account</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('AccountSettings')}>
+          <Ionicons name="settings" size={30} color="black" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.content}>
+        <View style={styles.inputContainer}>
+          <MaterialIcons name="account-circle" size={24} color="black" />
+          <Text style={styles.input}>{username}</Text>
+        </View>
+
+        <Text style={styles.subtitle}>Transaction list</Text>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => {}}>
+            <MaterialIcons name="pending-actions" size={20} color="black" />
+            <Text>Awaiting Payments</Text>
           </TouchableOpacity>
-      </View>
-
-      <View style={styles.inputContainer}>
-        <MaterialIcons name="account-circle" size={24} color="black" />
-        <Text style={styles.input}>{username}</Text>
-      </View>
-
-      {/* <TouchableOpacity style={styles.option}>
-        <MaterialIcons name="verified-user" size={24} color="black" />
-        <Text>Verified Profile</Text>
-        <Ionicons name="chevron-forward" size={24} color="black" />
-      </TouchableOpacity> */}
-    
-      <Text style={styles.subtitle}>Transaction list</Text>
-
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
-          <MaterialIcons name="pending-actions" size={20} color="black" />
-          <Text>Awaiting Payments</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
-          <MaterialIcons name="autorenew" size={20} color="black" />
-          <Text>Transaction in Progress</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
-          <MaterialIcons name="list" size={20} color="black" />
-          <Text>All transactions</Text>
-        </TouchableOpacity>
-      </View>
-
-      <TouchableOpacity style={styles.option} onPress={() => navigation.navigate('DataForSaleOverview')}>
-        <Text>My Data for Sale</Text>
-        <Ionicons name="chevron-forward" size={24} color="black" />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.option} onPress={() => navigation.navigate('DataSellTransactions')}>
-        <Text>Data Sell Transactions</Text>
-        <Ionicons name="chevron-forward" size={24} color="black" />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.option} onPress={() => navigation.navigate('Contact')}>
-        <Text>Contact Support</Text>
-        <Ionicons name="chevron-forward" size={24} color="black" />
-      </TouchableOpacity>
-
-      <View style={styles.logoutContainer}>
-          <TouchableOpacity onPress={handleLogout}>
-              <MaterialIcons name="logout" size={24} color="black" />
+          <TouchableOpacity style={styles.button} onPress={() => {}}>
+            <MaterialIcons name="autorenew" size={20} color="black" />
+            <Text>Transaction in Progress</Text>
           </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => {}}>
+            <MaterialIcons name="list" size={20} color="black" />
+            <Text>All transactions</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.option} onPress={() => navigation.navigate('DataForSaleOverview')}>
+          <Text>My Data for Sale</Text>
+          <Ionicons name="chevron-forward" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.option} onPress={handleDataSellTransactions}>
+          <Text>Data Sell Transactions</Text>
+          <Ionicons name="chevron-forward" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.option} onPress={() => navigation.navigate('Contact')}>
+          <Text>Contact Support</Text>
+          <Ionicons name="chevron-forward" size={24} color="black" />
+        </TouchableOpacity>
       </View>
-  </View>
+
+      <TouchableOpacity style={styles.logoutContainer} onPress={handleLogout}>
+        <MaterialIcons name="logout" size={24} color="black" />
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 45,
-    paddingLeft: 15,
     backgroundColor: '#E0E0E0',
   },
-  titleContainer: {
+  header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom: 10,
-    paddingRight: 10,
+    backgroundColor: 'gray',
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    marginTop: 30,
   },
   title: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontSize: 25,
+    color: 'black',
+    textAlign: 'center',
+    flex: 1, 
+  },
+  content: {
+    padding: 15,
+    marginTop: 15,
   },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 20,
-    paddingRight:10,
   },
   input: {
     marginLeft: 10,
@@ -126,6 +138,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingVertical: 10,
     paddingHorizontal: 20,
+    backgroundColor: '#F4F4F4',
+    borderRadius: 10,
+    marginRight: 10,
   },
   option: {
     flexDirection: 'row',
